608090a839bada31f1de8329716070047b338581
Caleb Xu <calebcenter@live.com>
2022-12-31 15:02:35 -0500

    Set up eslint for use with project

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports =  {
+  ignorePatterns: ["/dist/**/*.js"],
+  overrides: [
+    {
+      files: [
+        "./**/*.ts"
+      ],
+      parser: "@typescript-eslint/parser",
+      extends:  [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parserOptions:  {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+      rules: {
+        "indent": ["error", 2]
+      },
+    },
+    {
+      files: [
+        "./**/*.tsx",
+      ],
+      parser: "@typescript-eslint/parser",
+      extends:  [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parserOptions:  {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        jsx: true,
+      },
+      rules: {
+        "indent": ["error", 2]
+      },
+    },
+    {
+      files: [
+        "./*.js"
+      ],
+      extends: [
+        'eslint:recommended',
+      ],
+      env: {
+        node: true
+      },
+      parserOptions:  {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+      rules: {
+        "indent": ["error", 2],
+        "no-unused-vars": "off",
+      }
+    },
+  ]
+};
