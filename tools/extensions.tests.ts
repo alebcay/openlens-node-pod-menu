8f9b1f83aee03bd8d85b41f6ecfa83565b86be4b
Caleb Xu <calebcenter@live.com>
2023-01-13 22:55:49 -0500

    Style/naming nits for tests

--- tools/extensions.tests.ts
+++ tools/extensions.tests.ts
@@ -16,7 +16,7 @@ describe("extensions page tests", () => {
     let app: ElectronApplication;
 
     ({ window, cleanup, app } = await utils.start());
-    window.on('console', msg => console.log(msg.text()))
+    window.on('console', msg => console.log(msg.text()));
     await utils.clickWelcomeButton(window);
 
     // Navigate to extensions page
@@ -30,23 +30,23 @@ describe("extensions page tests", () => {
 
     // Trigger extension install
     const textbox = window.getByPlaceholder("Name or file path or URL");
-    await textbox.fill(process.env.EXTENSION_PATH)
+    await textbox.fill(process.env.EXTENSION_PATH);
     const install_button_selector = 'button[class*="Button install-module__button--"]';
     await window.waitForSelector(install_button_selector.concat('[data-waiting=false]'));
-    await window.click(install_button_selector.concat('[data-waiting=false]'))
-    await window.waitForSelector(install_button_selector.concat('[data-waiting=true]'))
+    await window.click(install_button_selector.concat('[data-waiting=false]'));
+    await window.waitForSelector(install_button_selector.concat('[data-waiting=true]'));
     await window.waitForSelector(install_button_selector.concat('[data-waiting=false]'));
 
     // Expect extension to be listed in installed list and enabled
-    const installedExtensionName = await (await window.waitForSelector('div[class*="installed-extensions-module__extensionName--"]')).textContent()
-    expect(installedExtensionName).toBe("@alebcay/openlens-node-pod-menu")
-    const installedExtensionState = await (await window.waitForSelector('div[class*="installed-extensions-module__enabled--"]')).textContent()
-    expect(installedExtensionState).toBe("Enabled")
-    await window.click('div[class*="close-button-module__closeButton--"][aria-label=Close]')
+    const installedExtensionName = await (await window.waitForSelector('div[class*="installed-extensions-module__extensionName--"]')).textContent();
+    expect(installedExtensionName).toBe("@alebcay/openlens-node-pod-menu");
+    const installedExtensionState = await (await window.waitForSelector('div[class*="installed-extensions-module__enabled--"]')).textContent();
+    expect(installedExtensionState).toBe("Enabled");
+    await window.click('div[class*="close-button-module__closeButton--"][aria-label=Close]');
 
     // Navigate to catalog
-    await window.waitForSelector('div[data-rbd-draggable-id=catalog-entity]')
-    await window.click('div[data-rbd-draggable-id=catalog-entity]')
+    await window.waitForSelector('div[data-rbd-draggable-id=catalog-entity]');
+    await window.click('div[data-rbd-draggable-id=catalog-entity]');
 
     // Navigate to minikube cluster
     frame = await utils.lauchMinikubeClusterFromCatalog(window);
@@ -54,7 +54,7 @@ describe("extensions page tests", () => {
     // Expand workloads menu if not already expanded
     await frame.waitForSelector('div[data-testid=sidebar-item-workloads]');
     if (await frame.locator('div[data-testid=sidebar-item-pods]').count() === 0) {
-      await frame.click('div[data-testid=sidebar-item-workloads]')
+      await frame.click('div[data-testid=sidebar-item-workloads]');
     }
   }, 10*60*1000);
 
@@ -64,14 +64,14 @@ describe("extensions page tests", () => {
 
   it('adds menu items to the pod details overlay', async () => {
     // Navigate to pods view
-    await frame.click('div[data-testid=sidebar-item-pods]')
+    await frame.click('div[data-testid=sidebar-item-pods]');
 
     // Click on etcd-minikube pod
     await frame.waitForSelector('text=etcd-minikube');
-    await frame.click('text=etcd-minikube')
+    await frame.click('text=etcd-minikube');
 
     // Wait for pod details to pop out
-    await frame.waitForSelector('span[data-icon-name=close]')
+    await frame.waitForSelector('span[data-icon-name=close]');
 
     // Check for expected menu items
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Attach Pod"').count()).toBe(1);
@@ -79,19 +79,19 @@ describe("extensions page tests", () => {
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Logs"').count()).toBe(1);
 
     // Close pod details
-    await frame.click('span[data-icon-name=close]')
+    await frame.click('span[data-icon-name=close]');
   }, 10*60*1000);
 
   it('adds menu items to the pod actions dropdown', async () => {
     // Navigate to pods view
-    await frame.click('div[data-testid=sidebar-item-pods]')
+    await frame.click('div[data-testid=sidebar-item-pods]');
 
     // Sort pods by running status
-    await frame.click('div[id="status"][class="TableCell status nowrap sorting"]')
+    await frame.click('div[id="status"][class="TableCell status nowrap sorting"]');
 
     // Click on a running pod item's dropdown
-    const menu = frame.locator('div[class="TableCell menu"]').last()
-    await menu.click()
+    const menu = frame.locator('div[class="TableCell menu"]').last();
+    await menu.click();
 
     // Wait for dropdown menu to appear
     await frame.waitForSelector('ul[id*="menu-actions-for-kube-object-menu-for-"]');
@@ -102,22 +102,22 @@ describe("extensions page tests", () => {
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Logs"').count()).toBe(1);
 
     // Close dropdown
-    await menu.click()
+    await menu.click();
 
     // Go back to sorting by name
-    await frame.click('div[id="name"][class="TableCell name nowrap sorting"]')
+    await frame.click('div[id="name"][class="TableCell name nowrap sorting"]');
   }, 10*60*1000);
 
   it('adds menu items to the node details overlay', async () => {
     // Navigate to nodes view
-    await frame.click('div[data-testid=sidebar-item-nodes]')
+    await frame.click('div[data-testid=sidebar-item-nodes]');
 
     // Click on minikube node
     await frame.waitForSelector('div[class="TableCell name"] >> text=minikube');
-    await frame.click('div[class="TableCell name"] >> text=minikube')
+    await frame.click('div[class="TableCell name"] >> text=minikube');
 
     // Wait for node details to pop out
-    await frame.waitForSelector('span[data-icon-name=close]')
+    await frame.waitForSelector('span[data-icon-name=close]');
 
     // Check for expected menu items
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Shell"').count()).toBe(1);
@@ -125,16 +125,16 @@ describe("extensions page tests", () => {
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Drain"').count()).toBe(1);
 
     // Close node details
-    await frame.click('span[data-icon-name=close]')
+    await frame.click('span[data-icon-name=close]');
   }, 10*60*1000);
 
   it('adds menu items to the node actions dropdown', async () => {
     // Navigate to nodes view
-    await frame.click('div[data-testid=sidebar-item-nodes]')
+    await frame.click('div[data-testid=sidebar-item-nodes]');
 
     // Click on a node item's dropdown
-    const menu = frame.locator('div[class="TableCell menu"]').first()
-    await menu.click()
+    const menu = frame.locator('div[class="TableCell menu"]').first();
+    await menu.click();
 
     // Wait for dropdown menu to appear
     await frame.waitForSelector('ul[id*="menu-actions-for-kube-object-menu-for-"]');
@@ -145,6 +145,6 @@ describe("extensions page tests", () => {
     expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Drain"').count()).toBe(1);
 
     // Close dropdown
-    await menu.click()
+    await menu.click();
   }, 10*60*1000);
 });
