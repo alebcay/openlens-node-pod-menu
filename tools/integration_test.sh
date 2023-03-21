e528144e46861d7caffaa4556ba8909892be2e70
Caleb Xu <calebcenter@live.com>
2023-03-21 13:21:21 -0400

    Unify test scripts

--- tools/integration_test.sh
+++ tools/integration_test.sh
@@ -4,25 +4,36 @@ set -exo pipefail
 
 [ -z "${EXTENSION_PATH}" ] && echo "EXTENSION_PATH environment variable must be set" && exit 1
 
-DIR=$(mktemp -d)
-git clone https://github.com/lensapp/lens ${DIR}/lens
-[ ! -z "${LENS_REF}" ] && git -C ${DIR}/lens checkout ${LENS_REF}
-cp tools/extensions.tests.ts ${DIR}/lens/packages/open-lens/integration/__tests__/extensions.tests.ts
-TARGET_FILE="${DIR}/lens/packages/open-lens/package.json" node tools/remove_extra_lens_targets.js
+TEST_TMPDIR=$(mktemp -d)
+git clone https://github.com/lensapp/lens ${TEST_TMPDIR}/lens
+[ ! -z "${LENS_REF}" ] && git -C ${TEST_TMPDIR}/lens checkout ${LENS_REF}
 
-pushd ${DIR}/lens
-    if [ -f "package-lock.json" ]; then
-        npm ci
+if [ -d "${TEST_TMPDIR}/lens/packages/open-lens" ]; then
+    OPENLENS_ROOT_DIR="${TEST_TMPDIR}/lens/packages/open-lens"
+else
+    OPENLENS_ROOT_DIR="${TEST_TMPDIR}/lens"
+fi
+
+cp tools/extensions.tests.ts "${OPENLENS_ROOT_DIR}/integration/__tests__/extensions.tests.ts"
+TARGET_FILE="${OPENLENS_ROOT_DIR}/package.json" node tools/remove_extra_lens_targets.js
+
+pushd ${TEST_TMPDIR}/lens
+    if [ -f "Makefile" ]; then
+        make build
     else
-        npm install
+        if [ -f "package-lock.json" ]; then
+            npm ci
+        else
+            npm install
+        fi
+
+        npm run build:app
     fi
 
-    npm run build:app
-
     # If left present, the snap package will be mistaken for an obsolete Jest snapshot
-    rm -f packages/open-lens/dist/*.snap
+    rm -f ${OPENLENS_ROOT_DIR}/dist/*.snap
 
-    cd packages/open-lens && npx jest --forceExit -- integration/__tests__/extensions.tests.ts
+    cd ${OPENLENS_ROOT_DIR} && npx jest --forceExit -- integration/__tests__/extensions.tests.ts
 popd
 
-rm -rf ${DIR}
+rm -rf ${TEST_TMPDIR}
