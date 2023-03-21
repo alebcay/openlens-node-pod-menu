06e54741f05b746fdb230587e0b7a7a70b4bd00d
Caleb Xu <calebcenter@live.com>
2023-03-21 18:16:19 -0400

    Style and lint nits

--- tools/integration_test.sh
+++ tools/integration_test.sh
@@ -4,9 +4,9 @@ set -exo pipefail
 
 [ -z "${EXTENSION_PATH}" ] && echo "EXTENSION_PATH environment variable must be set" && exit 1
 
-TEST_TMPDIR=$(mktemp -d)
-git clone https://github.com/lensapp/lens ${TEST_TMPDIR}/lens
-[ ! -z "${LENS_REF}" ] && git -C ${TEST_TMPDIR}/lens checkout ${LENS_REF}
+TEST_TMPDIR="$(mktemp -d)"
+git clone https://github.com/lensapp/lens "${TEST_TMPDIR}/lens"
+[ -n "${LENS_REF}" ] && git -C "${TEST_TMPDIR}/lens" checkout "${LENS_REF}"
 
 if [ -d "${TEST_TMPDIR}/lens/packages/open-lens" ]; then
     OPENLENS_ROOT_DIR="${TEST_TMPDIR}/lens/packages/open-lens"
@@ -17,7 +17,7 @@ fi
 cp tools/extensions.tests.ts "${OPENLENS_ROOT_DIR}/integration/__tests__/extensions.tests.ts"
 TARGET_FILE="${OPENLENS_ROOT_DIR}/package.json" node tools/remove_extra_lens_targets.js
 
-pushd ${TEST_TMPDIR}/lens
+pushd "${TEST_TMPDIR}/lens"
     if [ -f "Makefile" ]; then
         make build
     else
@@ -31,9 +31,9 @@ pushd ${TEST_TMPDIR}/lens
     fi
 
     # If left present, the snap package will be mistaken for an obsolete Jest snapshot
-    rm -f ${OPENLENS_ROOT_DIR}/dist/*.snap
+    rm -f "${OPENLENS_ROOT_DIR}"/dist/*.snap
 
-    cd ${OPENLENS_ROOT_DIR} && npx jest --forceExit -- integration/__tests__/extensions.tests.ts
+    cd "${OPENLENS_ROOT_DIR}" && npx jest --forceExit -- integration/__tests__/extensions.tests.ts
 popd
 
-rm -rf ${TEST_TMPDIR}
+rm -rf "${TEST_TMPDIR}"
